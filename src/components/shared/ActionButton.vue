a01bfac53d26edb40da545e7370ec7d2a1d5b0be
Pelumi Majolagbe <pelumi@bejamas.io>
2022-03-13 17:01:28 +0100

    feat: create reuseable button component

---
+++ src/components/shared/ActionButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="btn">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  background: var(--clr-accent-purple);
+  transition: all 0.3s;
+  padding: 0.7em 1em;
+  border-radius: 20rem;
+  cursor: pointer;
+
+  &:hover {
+    background: var(--clr-accent-purple-hover);
+  }
+}
+</style>
