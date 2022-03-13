3730df7eaffddb1ee5df1201117f36a965bc8c9a
Pelumi Majolagbe <pelumi@bejamas.io>
2022-03-13 17:01:58 +0100

    feat: use invoice header and empty comp in home

--- src/views/InvoiceHome.vue
+++ src/views/InvoiceHome.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="home">
-    <h1>This is the home page</h1>
-  </div>
+  <section class="home">
+    <app-invoice-header></app-invoice-header>
+    <app-empty-invoice></app-empty-invoice>
+  </section>
 </template>
+
+<script>
+import InvoiceHeader from "@/components/invoice/InvoiceHeader";
+import EmptyInvoice from "@/components/invoice/EmptyInvoice";
+export default {
+  name: "invoiceHome",
+  components: {
+    "app-invoice-header": InvoiceHeader,
+    "app-empty-invoice": EmptyInvoice,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  width: min(95%, 90rem);
+  margin: 5rem auto;
+  min-height: calc(100vh - 10rem);
+}
+</style>
