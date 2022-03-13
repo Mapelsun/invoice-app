1ba35e6522c41a8a21816a825fedf212f96b9162
Pelumi Majolagbe <pelumi@bejamas.io>
2022-03-13 07:35:46 +0100

    style: add google font and color variables

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,27 @@
 </template>
 
 <style lang="scss">
+// Font import
+@import url("https://fonts.googleapis.com/css2?family=Spartan:wght@100;300;400;500;700&display=swap");
+
+// Global variables
+:root {
+  --clr-primary: #7c5dfa;
+  --clr-primary-hover: #9277ff;
+  --clr-secondary: #ec5757;
+  --clr-secondary-hover: rgb(255, 151, 151);
+
+  // --clr-purple-dark: #1e2139;
+  // --clr-purple-light: #252945;
+  // --clr-purple-dark: #dfe3fa;
+  // --clr-purple-light: #888eb0;
+  // --clr-purple-dark: #7e88c3;
+  // --clr-purple-light: #0c0e16;
+
+  --clr-bg-light: #f8f8fb;
+  --clr-bg-dark: #141625;
+}
+
 // Basic default global reset
 html {
   box-sizing: border-box;
@@ -23,6 +44,8 @@ html {
 body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-family: "Spartan", sans-serif;
+  font-weight: 400;
   font-size: 1.6rem;
 }
 
@@ -31,5 +54,9 @@ input,
 textarea,
 select {
   font: inherit;
+}
+
+img {
+  width: 100%;
 }
 </style>
