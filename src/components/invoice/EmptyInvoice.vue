7b0f76c78fb5bae5dc39041881318fc0ffa8b789
Pelumi Majolagbe <pelumi@bejamas.io>
2022-03-13 17:00:44 +0100

    feat: create empty invoice component

---
+++ src/components/invoice/EmptyInvoice.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="empty">
+    <img
+      class="empty__image"
+      src="@/assets/illustration-empty.svg"
+      alt="empty illustration"
+    />
+    <div class="empty__details">
+      <h3>There is nothing here</h3>
+      <p>
+        Create an invoice by clicking the<br />
+        <span>New Invoice</span> button to get started
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.empty {
+  flex: 1;
+  display: grid;
+  place-content: center;
+  &__image {
+    justify-self: center;
+    margin-bottom: 5rem;
+  }
+  &__details {
+    text-align: center;
+    h3 {
+      margin-bottom: 2rem;
+    }
+    p {
+      font-size: 1.2rem;
+    }
+    span {
+      font-weight: bold;
+      cursor: pointer;
+    }
+  }
+}
+</style>
