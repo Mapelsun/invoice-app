6e9f62038cb460a583a47502b4eff2ad51248e67
Pelumi Majolagbe <pelumi@bejamas.io>
2022-03-13 21:28:24 +0100

    feat: create status pill as a component

--- src/components/invoice/InvoiceItem.vue
+++ src/components/invoice/InvoiceItem.vue
@@ -4,7 +4,7 @@
     <p class="inv__date">Due &nbsp;{{ "2021-08-19" | formatDate }}</p>
     <p class="inv__name">Jensen Huang</p>
     <p class="inv__price"><span>&#163;</span>1800.90</p>
-    <div class="inv__status"><span></span>{{ "paid" | capitalize }}</div>
+    <app-status-pill stat="paid"></app-status-pill>
     <img
       class="inv__image"
       src="@/assets/icon-arrow-right.svg"
@@ -14,8 +14,12 @@
 </template>
 
 <script>
+import StatusPill from "@/components/shared/StatusPill";
 export default {
   name: "invoiceItem",
+  components: {
+    "app-status-pill": StatusPill,
+  },
 };
 </script>
 
@@ -57,27 +61,6 @@ export default {
   &__price {
     font-size: 1.8rem;
     text-align: right;
-  }
-  &__status {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-width: 13rem;
-    gap: 1rem;
-    background: rgba(green, 0.1);
-    color: green;
-    border-radius: 0.5rem;
-    padding: 0.8em 1.2em;
-    font-size: 1.6rem;
-    letter-spacing: 0.1;
-    font-weight: bold;
-    span {
-      display: inline-block;
-      width: 1rem;
-      height: 1rem;
-      border-radius: 50%;
-      background: green;
-    }
   }
   &__image {
     transition: 0.2s linear;
---
+++ src/components/shared/StatusPill.vue
@@ -0,0 +1,70 @@
+<template>
+  <div
+    class="pill"
+    :class="
+      stat === 'paid'
+        ? 'paid'
+        : stat === 'pending'
+        ? 'pending'
+        : stat === 'draft'
+        ? 'draft'
+        : ''
+    "
+  >
+    <span></span>{{ stat | capitalize }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "statusPill",
+  props: {
+    stat: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.pill {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 13rem;
+  gap: 1rem;
+  border-radius: 0.5rem;
+  padding: 0.8em 1.2em;
+  font-size: 1.6rem;
+  letter-spacing: 0.1;
+  span {
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+  }
+}
+
+.paid {
+  background: rgba(#008000, 0.1);
+  color: #008000;
+  span {
+    background: #008000;
+  }
+}
+.pending {
+  background: rgba(#d17d38, 0.1);
+  color: #d17d38;
+  span {
+    background: #d17d38;
+  }
+}
+.draft {
+  background: rgba(#ffffff, 0.1);
+  color: #ffffff;
+  span {
+    background: #ffffff;
+  }
+}
+</style>
