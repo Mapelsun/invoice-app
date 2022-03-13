8a6ba77718d49aa17f3a073fb1260874f6d0c772
Pelumi Majolagbe <pelumi@bejamas.io>
2022-03-13 20:58:22 +0100

    feat: create capitalize and formatDate filters

--- src/main.js
+++ src/main.js
@@ -3,12 +3,29 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Importing Dayjs
+import dayjs from "dayjs";
+
 // Layouts
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import HomeLayout from "@/layouts/HomeLayout.vue";
 
 Vue.component("default-layout", DefaultLayout);
 Vue.component("home-layout", HomeLayout);
+
+Vue.filter("capitalize", (value) => {
+  return value
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+});
+
+Vue.filter("formatDate", (value) => {
+  if (value) {
+    return dayjs(String(value)).format("DD MMM YYYY");
+  }
+});
 
 Vue.config.productionTip = false;
 
