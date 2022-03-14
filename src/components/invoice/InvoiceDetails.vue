d944382688b4e25acd962480aa2c4083fb9ed4c1
Pelumi Majolagbe <pelumi@bejamas.io>
2022-03-14 10:35:31 +0100

    feat: create modal comp with style on click of del

--- src/components/invoice/InvoiceDetails.vue
+++ src/components/invoice/InvoiceDetails.vue
@@ -1,25 +1,46 @@
 <template>
-  <section class="details">
-    <app-back-nav></app-back-nav>
-    <div class="details__actions">
-      <div>
-        <p>Status</p>
-        <app-status-pill :stat="invoice.status"></app-status-pill>
+  <div>
+    <section class="details">
+      <app-back-nav></app-back-nav>
+      <div class="details__actions">
+        <div>
+          <p>Status</p>
+          <app-status-pill :stat="invoice.status"></app-status-pill>
+        </div>
+        <div>
+          <app-button type="edit">
+            <p>Edit</p>
+          </app-button>
+          <app-button type="delete" @click.native="showModal = true">
+            <p>Delete</p>
+          </app-button>
+          <app-button type="default">
+            <p>Mark as Paid</p>
+          </app-button>
+        </div>
       </div>
-      <div>
-        <app-button type="edit">
-          <p>Edit</p>
-        </app-button>
-        <app-button type="delete">
-          <p>Delete</p>
-        </app-button>
-        <app-button type="default">
-          <p>Mark as Paid</p>
-        </app-button>
+      <app-invoice-info :invoice="invoice"></app-invoice-info>
+    </section>
+    <app-modal v-if="showModal" @close="showModal = false">
+      <div class="delete">
+        <div class="delete__text">
+          <h2>Confirm Deletion</h2>
+          <p class="p-two">
+            Are you sure you want to delete <span>&#35;</span>{{ invoice.id }}?
+            This action cannot be undone.
+          </p>
+        </div>
+        <div class="delete__btns">
+          <app-button type="edit" @click.native="showModal = false">
+            <p>Cancel</p>
+          </app-button>
+          <app-button type="delete">
+            <p>Delete</p>
+          </app-button>
+        </div>
       </div>
-    </div>
-    <app-invoice-info :invoice="invoice"></app-invoice-info>
-  </section>
+    </app-modal>
+  </div>
 </template>
 
 <script>
@@ -28,6 +49,7 @@ import BackNav from "@/components/shared/BackNav";
 import StatusPill from "@/components/shared/StatusPill";
 import ActionButton from "@/components/shared/ActionButton";
 import InvoiceInfo from "@/components/invoice/InvoiceInfo";
+import AppModal from "@/components/widgets/AppModal";
 export default {
   name: "invoiceDetails",
   computed: mapState(["invoice"]),
@@ -36,6 +58,12 @@ export default {
     "app-status-pill": StatusPill,
     "app-button": ActionButton,
     "app-invoice-info": InvoiceInfo,
+    "app-modal": AppModal,
+  },
+  data() {
+    return {
+      showModal: false,
+    };
   },
 };
 </script>
@@ -63,6 +91,24 @@ export default {
       align-items: center;
       gap: 1rem;
     }
+  }
+}
+
+.delete {
+  &__text {
+    margin-bottom: 2rem;
+    h2 {
+      margin-bottom: 1.5rem;
+    }
+    span {
+      font-size: 1.1rem;
+      color: var(--clr-grey-two);
+    }
+  }
+  &__btns {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1rem;
   }
 }
 </style>
---
+++ src/components/widgets/AppModal.vue
@@ -0,0 +1,59 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-container">
+        <div class="modal-body">
+          <slot> </slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "appModal",
+};
+</script>
+
+<style lang="scss" scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: opacity 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-container {
+  position: relative;
+  max-width: 50rem;
+  width: 90%;
+  margin: 0px auto;
+  padding: 5rem 6rem;
+  background: var(--clr-bg-dark-three);
+  border-radius: 0.75rem;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
