f2edd502a36ca18ac6c6da5d10392ce84c84494c
Pelumi Majolagbe <pelumi@bejamas.io>
2022-03-15 17:40:40 +0100

    feat: add addInvoice endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -7,6 +7,11 @@ export default {
     let url = config.baseUrl + "/invoices";
     return axios({ method, url });
   },
+  addInvoice: (data) => {
+    let method = "post";
+    let url = config.baseUrl + "/invoices";
+    return axios({ method, url, data });
+  },
   deleteInvoice: (id) => {
     let method = "delete";
     let url = config.baseUrl + `/invoices/${id}`;
